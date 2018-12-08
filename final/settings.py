55215cab6a295e896c2a6c4c74de9e8d291136dc
tkh67320 <tkh67320@uga.edu>
2018-12-08 18:32:19 -0500

    added css styling

--- final/settings.py
+++ final/settings.py
@@ -120,4 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/final/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
