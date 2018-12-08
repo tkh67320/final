ca005409e395229a46365f6c11de11030b671c56
tkh67320 <tkh67320@uga.edu>
2018-12-08 18:27:11 -0500

    admin url updated

--- final/urls.py
+++ final/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('final/', include('busBuddy.urls')),
-    path('admin/', admin.site.urls),
+    path('final/admin/', admin.site.urls),
 ]
