c0bb714853dbc095f7f1900f7de2cc7f146ae3e9
brandonkim287 <bk67762@uga.edu>
2018-12-07 21:50:42 -0500

    Added screen command

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /home/student/projects/final/bin/activate
+screen uwsgi --socket 127.0.0.1:4305 \
+--venv /home/student/projects/final \
+--wsgi-file /home/student/projects/final/wsgi.py \
+--master
