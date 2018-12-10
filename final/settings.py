b354aba2f54839837a60b32c1c9abb446300b771
brandonkim287 <bk67762@uga.edu>
2018-12-10 16:31:00 -0500

    attempted to set up templates

--- final/settings.py
+++ final/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'final.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'busses',
         'USER': 'johndoe',
         'PASSWORD': 'Johndoe123!',
